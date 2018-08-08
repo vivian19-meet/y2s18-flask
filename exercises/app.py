020c5e5c9185297fc3c0d5146771e0a7feacb6e0
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 12:09:46 +0300

    don

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
 @app.route('/')
 def home_page():
-    print("")
-    return render_template("index.html")
+    fav=["noam","shelly","daria","marah","viva","leen"]
+    return render_template("index.html",fav=fav,likes_same_sport=True)
 if __name__ == '__main__':
     app.run(debug = True)
